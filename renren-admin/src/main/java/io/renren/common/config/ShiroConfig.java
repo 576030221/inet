1d2938820db33c68c3c4a4a13d7de8c4b20255fc
cyh <576030221@qq.com>
2019-11-09 12:43:13 +0800

    添加课程时，时间的选择已经完成了

--- renren-admin/src/main/java/io/renren/common/config/ShiroConfig.java
+++ renren-admin/src/main/java/io/renren/common/config/ShiroConfig.java
@@ -89,7 +89,7 @@ public class ShiroConfig {
         filterMap.put("/sys/user/regist", "anon");
         filterMap.put("/favicon.ico", "anon");
         filterMap.put("/captcha.jpg", "anon");
-//        filterMap.put("/**", "authc");
+        filterMap.put("/**", "authc");
         shiroFilter.setFilterChainDefinitionMap(filterMap);
 
         return shiroFilter;
